53eb5e12bde037454a6938b934e6ec22b8d9ba03
Nokvix <igor.gass2003@mail.ru>
2024-12-06 21:47:49 +0500

    Немного переделал

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['10.0.2.15', '127.0.0.1', 'localhost', 'kittydev.ddns.net']
+ALLOWED_HOSTS = ['172.30.49.186', '127.0.0.1', 'localhost', 'kittydev.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
